4e497900a006e5c3156d0e9404f2f41d58a74f66
Galo Sandoval <galo.sandoval@ibm.com>
2021-09-30 21:25:41 -0700

    finishes main functionality with hardcoded values

--- src/features/instructions/Instructions.jsx
+++ src/features/instructions/Instructions.jsx
@@ -1,11 +1,63 @@
 import React from "react";
+import { useEffect } from "react";
 
-export const Instructions = ({ time, setTime }) => {
-  console.log("time", time);
-  const instructions = () => {
-    if (time.seconds === 2) {
-      console.log("Hey its 2");
+// 4, 11, 19, 23, 30, 38, 42
+
+export const Instructions = ({
+  breatheInstructions,
+  setBreatheInstructions,
+  setTime,
+  initialTimeState,
+  time
+}) => {
+  useEffect(() => {
+    if (time.isActive) {
+      const { stepsCounter, step, repeatCount, cycleCount } = breatheInstructions;
+
+      if (stepsCounter === 0 && step === 0) {
+        setBreatheInstructions({
+          ...breatheInstructions,
+          step: 1,
+          cycleCount: (breatheInstructions.cycleCount += 1)
+        });
+        if (breatheInstructions.instruction === "Repeat")
+          setBreatheInstructions({ ...breatheInstructions, instruction: "Breathe In", step: 1 });
+      } else if (stepsCounter === 4 && step === 1) {
+        setBreatheInstructions({
+          ...breatheInstructions,
+          step: 2,
+          instruction: "Hold Your Breathe",
+          stepsCounter: 0
+        });
+      } else if (stepsCounter === 7 && step === 2) {
+        setBreatheInstructions({
+          ...breatheInstructions,
+          step: 3,
+          instruction: "Breathe Out",
+          stepsCounter: 0
+        });
+      } else if (stepsCounter === 7 && step === 3 && repeatCount > cycleCount) {
+        setBreatheInstructions({
+          ...breatheInstructions,
+          step: 0,
+          stepsCounter: -1,
+          instruction: "Repeat"
+        });
+      } else if (stepsCounter === 8 && repeatCount === cycleCount) {
+        setBreatheInstructions({ ...breatheInstructions, instruction: "End" });
+        setTime(initialTimeState);
+      }
+      console.log(
+        "step",
+        step,
+        "counter",
+        stepsCounter,
+        "cycleCount",
+        cycleCount,
+        "repeatCount",
+        repeatCount
+      );
     }
-  };
-  return <div>{instructions}</div>;
+  }, [breatheInstructions, setBreatheInstructions, setTime, initialTimeState, time]);
+  return <div>{breatheInstructions.instruction}</div>;
 };
